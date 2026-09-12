857263824a8efdd21b81135e93d1a84a8313f02d
“Anna <pili.anna@yahoo.com>
2026-09-12 11:36:44 +0200

    initial server structure

---
+++ src/data/gameData.js
@@ -0,0 +1,24 @@
+export const gameData = {
+  title: "A treasure hunt for Dani",
+
+  physical: {
+    frenchPhrase: "...",
+    prompt: "...",
+    accepted: ["21.08"],
+  },
+
+  coordinates: {
+    targetLat: 21.08,
+    targetLng: 19.85,
+    revealedWord: "CHAD",
+  },
+
+  cities: [
+    {
+      id: "darmstadt",
+      city: "DARMSTADT",
+      prompt: "...",
+      accepted: ["darmstadt"],
+    },
+  ],
+};
